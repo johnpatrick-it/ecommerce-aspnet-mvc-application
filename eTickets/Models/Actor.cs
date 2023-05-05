fc5917294a79a41627673b585bb2232b1f457002
johnpatrick-it <johnpatrickhaguimit@gmail.com>
2023-05-05 10:05:18 +0800

    Add project files.

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string Fullname { get; set; }
+        public string  Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actor_Movies { get; set; }
+    }
+}
